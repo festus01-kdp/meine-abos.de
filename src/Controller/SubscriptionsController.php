fde05c28e2bdb4fe94851e18dcc11503e4e49fe9
Klaus-Dieter Pernak <klaus-dieter.pernak@gmx.de>
2022-03-07 13:27:02 +0100

    Push to Webspace

--- src/Controller/SubscriptionsController.php
+++ src/Controller/SubscriptionsController.php
@@ -1,16 +1,18 @@
 <?php
+
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class SubscriptionsController
 {
     /**
-     * @Route("/subscriptions", name="subscriptions")
+     * @Route("/subscriptions", name="list")
      */
-    public function list(): Response
+    public function list(Request $request): Response
     {
-        
+        // var_dump($request);
         return new Response('list subscritions');
     }
 }
